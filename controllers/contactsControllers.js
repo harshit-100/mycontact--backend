c01b03fd6cadd35c8536c28c94ee650c4cc00a31
harshit-100 <harshitsharma22462@gmail.com>
2023-02-25 00:53:25 +0530

    Performing crud operations

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,26 +1,28 @@
+const asyncHandler = require("express-async-handler");
+
 //@desc Get all contacts
 //@route GET /api/contacts
 //@access public
 
-const getContacts = (req, res) => {
+const getContacts = asyncHandler((req, res) => {
   res.status(200).json({ message: "get method is called" });
-};
+});
 
 //@desc update contacts
 //@route PUT /api/contacts/:id
 //@access public
 
-const getContact = (req, res) => {
+const getContact = asyncHandler((req, res) => {
   res
     .status(200)
     .json({ message: `get method is called on id ${req.params.id}` });
-};
+});
 
 //@desc create contacts
 //@route Post /api/contacts/:id
 //@access public
 
-const createContact = (req, res) => {
+const createContact = asyncHandler((req, res) => {
   console.log(req.body);
   const { name, email, phone } = req.body;
   if (!name || !email || !phone) {
@@ -28,27 +30,27 @@ const createContact = (req, res) => {
     throw new Error("all finds are mandotry!");
   }
   res.status(201).json({ message: `create method is called` });
-};
+});
 
 //@desc edit contacts
 //@route PUT /api/contacts/:id
 //@access public
 
-const updateContact = (req, res) => {
+const updateContact = asyncHandler((req, res) => {
   res
     .status(200)
     .json({ message: `put method is called on id ${req.params.id}` });
-};
+});
 
 //@desc delete contacts
 //@route Delete /api/contacts/:id
 //@access public
 
-const deleteContact = (req, res) => {
+const deleteContact = asyncHandler((req, res) => {
   res
     .status(201)
     .json({ message: `deleted method is called on id ${req.params.id}` });
-};
+});
 
 module.exports = {
   getContacts,
--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,21 +1,50 @@
+const { constants } = require("../constants");
+
 const errorHandler = (err, req, res, next) => {
   const statusCode = res.statusCode ? res.statusCode : 500;
   switch (statusCode) {
-    case 400:
+    case constants.VALIDATION_ERROR:
       res.json({
         title: "Validation Faild",
         message: err.message,
         stackTrace: err.stack,
       });
       break;
-    case 404:
+
+    case constants.NOT_FOUND:
       res.json({
         title: "Not Found",
         message: err.message,
         stackTrace: err.stack,
       });
       break;
+
+    case constants.FORBIDDEN:
+      res.json({
+        title: "forbidden",
+        message: err.message,
+        stackTrace: err.stack,
+      });
+      break;
+
+    case constants.UNAUTHORIZED:
+      res.json({
+        title: "un authorized",
+        message: err.message,
+        stackTrace: err.stack,
+      });
+      break;
+
+    case constants.SERVER_EORROR:
+      res.json({
+        title: "server error",
+        message: err.message,
+        stackTrace: err.stack,
+      });
+      break;
+
     default:
+      console.log("No Error All Good !");
       break;
   }
 };
