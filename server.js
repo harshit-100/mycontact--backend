bb98bc1e0bb174bc77444adb62d6714d38e44306
harshit-100 <harshitsharma22462@gmail.com>
2023-02-25 17:06:47 +0530

    add user route

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
+app.use("/api/users", require("./routes/userRoute"));
 app.use("/api/contacts", require("./routes/contactsRoute"));
 app.use(errorHandler);
 
