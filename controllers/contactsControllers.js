d936fbda33fa3530cb5ecff15e1db9eee38b23dd
harshit-100 <harshitsharma22462@gmail.com>
2023-02-25 16:23:32 +0530

    perfing crud operation using databases

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,55 +1,80 @@
 const asyncHandler = require("express-async-handler");
+const { default: mongoose } = require("mongoose");
+const Contact = require("../models/contactModel");
 
 //@desc Get all contacts
 //@route GET /api/contacts
 //@access public
 
-const getContacts = asyncHandler((req, res) => {
-  res.status(200).json({ message: "get method is called" });
+const getContacts = asyncHandler(async (req, res) => {
+  const contacts = await Contact.find();
+  res.status(200).json(contacts);
 });
 
 //@desc update contacts
 //@route PUT /api/contacts/:id
 //@access public
 
-const getContact = asyncHandler((req, res) => {
-  res
-    .status(200)
-    .json({ message: `get method is called on id ${req.params.id}` });
+const getContact = asyncHandler(async (req, res) => {
+  const contact = await Contact.findById(req.params.id);
+  if (!contact) {
+    res.status(404);
+    throw new Error("contact not found");
+  }
+  res.status(200).json(contact);
 });
 
 //@desc create contacts
 //@route Post /api/contacts/:id
 //@access public
 
-const createContact = asyncHandler((req, res) => {
-  console.log(req.body);
+const createContact = asyncHandler(async (req, res) => {
+  console.log("The requrest body is:", req.body);
   const { name, email, phone } = req.body;
   if (!name || !email || !phone) {
     res.status(400);
     throw new Error("all finds are mandotry!");
   }
-  res.status(201).json({ message: `create method is called` });
+
+  const contact = Contact.create({
+    name,
+    email,
+    phone,
+  });
+  res.status(201).json(contact);
 });
 
 //@desc edit contacts
 //@route PUT /api/contacts/:id
 //@access public
 
-const updateContact = asyncHandler((req, res) => {
-  res
-    .status(200)
-    .json({ message: `put method is called on id ${req.params.id}` });
+const updateContact = asyncHandler(async (req, res) => {
+  const contact = await Contact.findById(req.params.id);
+  if (!contact) {
+    res.status(404);
+    throw new Error("contact not found");
+  }
+
+  const updatedContact = await Contact.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    { new: true }
+  );
+  res.status(200).json(updatedContact);
 });
 
 //@desc delete contacts
 //@route Delete /api/contacts/:id
 //@access public
 
-const deleteContact = asyncHandler((req, res) => {
-  res
-    .status(201)
-    .json({ message: `deleted method is called on id ${req.params.id}` });
+const deleteContact = asyncHandler(async (req, res) => {
+  const contact = await Contact.findById(req.params.id);
+  if (!contact) {
+    res.status(404);
+    throw new Error("contact not found");
+  }
+  await Contact.remove();
+  res.status(200).json(contact);
 });
 
 module.exports = {
---
+++ models/contactModel.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const contactSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "please add the contact name"],
+    },
+    email: {
+      type: String,
+      required: [true, "please add the email id"],
+    },
+    phone: {
+      type: String,
+      required: [true, "please add the phone number"],
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Contact", contactSchema);
